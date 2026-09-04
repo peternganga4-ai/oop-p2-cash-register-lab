ff597dd30f23bc0c45451a03f392b793d3ec425c
Peter Ng'ang'a <peter.nganga4@student.moringaschool.com>
2026-09-04 21:28:46 +0300

    Add comments to cash register

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -10,34 +10,40 @@ class CashRegister:
 
 
     def add_item(self, item, price, quantity=1):
+            # add the total cost of the items to the register total
             self.total += price * quantity
-
+#add each item to items list based on quantity
             for _ in range(quantity):
                 self.items.append(item)
-
+#save the transaction so it can be voided later
                 self.previous_transactions.append({
                     "item": item,
                     "price": price,
                     "quantity": quantity
                 })
     def apply_discount(self):
+        #check on discout whether it is provided
         if self.discount == 0:
              print("There is no discount to apply.")
         else:
+             #calculate& subtract the discout from the total
              self.total = self.total - (self.total * self.discount / 100)
+             #Display the updated total
              print(f"After the discount, the total comes to ${self.total:.0f}.")
     def void_last_transaction(self):
+         # checks if there is a trasaction that can be voided
          if self.previous_transactions:
+              # Get and remove the most recent transaction
               last_transaction = self.previous_transactions.pop()
-
+#
               price = last_transaction["price"]
               quantity = last_transaction["quantity"]
-
+#substract the transaction from the total
               self.total -= price * quantity
-
+#remove the items from the the total
               for _ in range(quantity):
                    self.items.pop()
-                   
+
                    
                
 
